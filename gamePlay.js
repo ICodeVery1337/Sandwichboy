189f65744ea52ea487b5b08c27c9c0339daf9c9f
ITchaparov <iordan.tchaparov@cambridgeconsultants.com>
2026-01-23 14:24:42 +0000

    boundaries + falling sandwiches

--- gamePlay.js
+++ gamePlay.js
@@ -1,3 +1,6 @@
+
+const gameState = {}
+
 class gamePlay extends Phaser.Scene {
     constructor() {
         super("gamePlay");
@@ -5,16 +8,16 @@ class gamePlay extends Phaser.Scene {
 
     // The three methods currently empty
     hit() {
-        // Change the position x and y of the coin randomly
-        this.coin.x = Phaser.Math.Between(100, 1700);
-        this.coin.y = Phaser.Math.Between(100, 600);
+        // Change the position x and y of the sandwich randomly
+        this.sandwich1.x = Phaser.Math.Between(100, 1700);
+        this.sandwich1.y = Phaser.Math.Between(100, 600);
       
         // Increment the score by 10
         this.score += 10;
       
         // Display the updated score on the screen
         this.scoreText.setText('score: ' + this.score);
-
+        gameState.sfx.nice.play()
         // Create a new tween 
         // this.tweens.add({
         //     targets: this.player, // on the player 
@@ -25,29 +28,53 @@ class gamePlay extends Phaser.Scene {
         // });
       }
 
+      hit2() {
+        this.sandwich2.x = Phaser.Math.Between(100, 1700);
+        this.sandwich2.y = Phaser.Math.Between(100, 100);
+      
+ 
+        this.score += 10;
+        this.scoreText.setText('score: ' + this.score);
+        gameState.sfx.nice.play()
+      }
+
     preload() {
-        // this.load.image("player", "assets/player.png");
         this.load.image("player", "assets/Shades.png");
 
-        // this.load.image('coin', 'assets/coin.png');
-        this.load.image('coin', 'assets/Sandwich.png');
+        this.load.image('sandwich', 'assets/Sandwich.png');
 
         this.load.image('background', 'assets/board.png');
 
         this.load.audio("theme", "assets/sounds/JokingMotive.mp3" )
 
-        // this.load.audio("nice", "assets/sounds/Nice.mp3" )
+        this.load.audio("nice", "assets/sounds/Nice.mp3" )
 
 
     }
     create() {
+        gameState.sfx = {};
+        gameState.sfx.nice = this.sound.add('nice');
+
+        this.physics.world.setBounds(0, 0, 1800, 750); 
+
         this.add.image(400, 600, 'background');
 
         this.player = this.physics.add.sprite(100, 100, 'player');
         this.player.setScale(0.45)
+        this.player.setCollideWorldBounds(true);
 
-        this.coin = this.physics.add.sprite(300, 300, 'coin');
-        this.coin.setScale(0.3)
+        this.sandwich1 = this.physics.add.sprite(500, 500, 'sandwich');
+        this.sandwich1.setScale(0.3)
+
+        this.sandwich2 = this.physics.add.sprite(600, 100, 'sandwich');
+        this.sandwich2.setScale(0.3)
+        this.sandwich2.setGravityY(600)
+        this.sandwich2.setCollideWorldBounds(true)
+
+        this.sandwich3 = this.physics.add.sprite(700, 700, 'sandwich');
+        this.sandwich3.setScale(0.3)
+
+
 
 
         // Store the score in a variable, initialized at 0
@@ -69,10 +96,15 @@ class gamePlay extends Phaser.Scene {
         backgroundMusic.play();
     }
     update() {
-        // If the player is overlapping with the coin
-        if (this.physics.overlap(this.player, this.coin)) {
+        // If the player is overlapping with the sandwich
+        if (this.physics.overlap(this.player, this.sandwich1)) {
             // Call the new hit() method
             this.hit();
+        }
+
+        if (this.physics.overlap(this.player, this.sandwich2)) {
+            // Call the new hit() method
+            this.hit2();
         }
 
         // Handle horizontal movements
